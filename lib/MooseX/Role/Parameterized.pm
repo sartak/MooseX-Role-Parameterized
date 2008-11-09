19af6e75ef72d5d629227e9c980a6b97a4590743
Shawn M Moore <sartak@gmail.com>
2008-11-09 03:02:46 +0000

    parameter keyword

--- lib/MooseX/Role/Parameterized.pm
+++ lib/MooseX/Role/Parameterized.pm
@@ -7,7 +7,14 @@ use MooseX::Role::Parameterized::Meta::Role;
 use Moose::Role ();
 use Moose::Exporter;
 
-Moose::Exporter->setup_import_methods;
+Moose::Exporter->setup_import_methods(
+    with_caller => ['parameter'],
+);
+
+sub parameter {
+    my $caller = shift;
+    $caller->meta->add_parameter(@_);
+}
 
 sub init_meta {
     my $self = shift;
