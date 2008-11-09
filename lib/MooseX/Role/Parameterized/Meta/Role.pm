f563c3cda3fd8ce578f666294f4ca236e4c92505
Shawn M Moore <sartak@gmail.com>
2008-11-09 03:02:19 +0000

    We want a parameter_metaclass attribute; each role application will get an instance of the class it describes

--- lib/MooseX/Role/Parameterized/Meta/Role.pm
+++ lib/MooseX/Role/Parameterized/Meta/Role.pm
@@ -5,7 +5,21 @@ extends 'Moose::Meta::Role';
 
 use MooseX::Role::Parameterized::Parameters;
 
-sub parameter_class { 'MooseX::Role::Parameterized::Parameters' }
+has parameter_metaclass => (
+    is      => 'ro',
+    isa     => 'Moose::Meta::Class',
+    lazy    => 1,
+    default => sub {
+        Moose::Meta::Class->create_anon_class(
+            superclasses => ['MooseX::Role::Parameterized::Parameters'],
+        );
+    },
+);
+
+sub add_parameter {
+    my $self = shift;
+    $self->parameter_metaclass->add_attribute(@_);
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
