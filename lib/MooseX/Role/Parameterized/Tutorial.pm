93c2fd30a60cc71f9b06332a75e10433212ecf7a
Shawn M Moore <sartak@gmail.com>
2008-11-25 08:19:25 +0000

    Sketch out a tutorial :)

---
+++ lib/MooseX/Role/Parameterized/Tutorial.pm
@@ -0,0 +1,23 @@
+package MooseX::Role::Parameterized::Tutorial;
+confess "Don't use this module, read it!";
+
+__END__
+
+=head1 NAME
+
+MooseX::Role::Parameterized::Tutorial - why and how
+
+=head1 MOTIVATION
+
+=head1 USAGE
+
+=head3 C<with>
+
+=head3 C<parameter>
+
+=head3 C<role>
+
+=head1 IMPLEMENTATION NOTES
+
+=cut
+
