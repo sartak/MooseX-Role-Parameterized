7623a65e11d4190166bb8bc86efdc9c880c0c9f3
Shawn M Moore <sartak@gmail.com>
2008-11-25 06:23:30 +0000

    Remove kludges previously required because we didn't have anonymous roles

--- lib/MooseX/Role/Parameterized/Meta/Role/Parameterized.pm
+++ lib/MooseX/Role/Parameterized/Meta/Role/Parameterized.pm
@@ -4,32 +4,9 @@ use Moose;
 extends 'Moose::Meta::Role';
 
 has parameters => (
-    is       => 'rw',
-    isa      => 'MooseX::Role::Parameterized::Parameters',
-    required => 1,
+    is  => 'rw',
+    isa => 'MooseX::Role::Parameterized::Parameters',
 );
-
-# we override get_method_map because this is an anonymous role, there's no
-# package to check
-sub get_method_map {
-    my $self = shift;
-
-    return $self->{'methods'} ||= {};
-}
-
-# we override add_method because we don't want to install methods added through
-# this API; we just stick it in the method map
-sub add_method {
-    my ($self, $method_name, $method) = @_;
-    (defined $method_name && $method_name)
-    || Moose->throw_error("You must define a method name");
-
-    if (!blessed($method)) {
-        Moose->throw_error("You must pass a blessed method to add_method");
-    }
-
-    $self->get_method_map->{$method_name} = $method;
-}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
