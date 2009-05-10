888ca8e932514e60b4db4e79c4456161db023bd0
Shawn M Moore <sartak@gmail.com>
2009-05-10 12:01:23 -0400

    Doc for Parameter metaclass

--- lib/MooseX/Role/Parameterized/Meta/Parameter.pm
+++ lib/MooseX/Role/Parameterized/Meta/Parameter.pm
@@ -26,3 +26,17 @@ no Moose;
 
 1;
 
+__END__
+
+=head1 NAME
+
+MooseX::Role::Parameterized::Meta::Parameter - metaclass for parameters
+
+=head1 DESCRIPTION
+
+This is the metaclass for parameter objects, a subclass of
+L<Moose::Meta::Attribute>. Its sole purpose is to make the default value
+of the C<is> option C<ro>.
+
+=cut
+
