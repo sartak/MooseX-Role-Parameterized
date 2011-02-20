f2b5745341419b34a40db96068943c2a9062512a
Shawn M Moore <sartak@bestpractical.com>
2011-02-20 01:59:42 -0500

    Refactor the SEE ALSO section

--- lib/MooseX/Role/Parameterized.pm
+++ lib/MooseX/Role/Parameterized.pm
@@ -261,15 +261,19 @@ Shawn M Moore, C<sartak@gmail.com>
 
 =head1 SEE ALSO
 
-L<http://sartak.blogspot.com/2009/05/parameterized-roles.html>
+L<http://sartak.org/2009/01/parametric-roles-in-perl-5.html>
+
+L<http://sartak.org/2009/05/the-design-of-parameterized-roles.html>
 
 L<http://stevan-little.blogspot.com/2009/07/thoughts-on-parameterized-roles.html>
-
-L<http://sartak.org/talks/yapc-asia-2009/(parameterized)-roles/>
 
 L<http://perldition.org/articles/Parameterized%20Roles%20with%20MooseX::Declare.pod>
 
 L<http://www.modernperlbooks.com/mt/2011/01/the-parametric-role-of-my-mvc-plugin-system.html>
+
+L<http://sartak.org/talks/yapc-asia-2009/(parameterized)-roles/>
+
+L<https://github.com/SamuraiJack/JooseX-Role-Parameterized> - this extension ported to JavaScript's Joose
 
 =head1 COPYRIGHT AND LICENSE
 
